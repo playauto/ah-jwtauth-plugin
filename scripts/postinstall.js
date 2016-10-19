0311eb34de828df2fee2ec10b96c100102d593ce
Matthew Kim <byseob@naver.com>
2016-10-19 14:55:32 +0900

    Update postinstall.js
    force postinstall script errors

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,7 +6,10 @@ var path = require('path');
 var srcFile   = path.normalize(__dirname + '/../config/jwtauth.js');
 var destFile = path.normalize(process.cwd() + '/../../config/jwtauth.js');
 
-if(!fs.existsSync(destFile)) {
-  console.log("Copying " + srcFile + " to " + destFile);
-  fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
+try {
+  if(!fs.existsSync(destFile)) {
+    console.log("Copying " + srcFile + " to " + destFile);
+    fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
+  }
 }
+catch(e) {}
